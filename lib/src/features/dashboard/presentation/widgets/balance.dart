2e0b3658d34c35a4fbcece521323dd85464b439f
Code On Saturday <devfloody@gmail.com>
2025-02-27 05:33:53 +0700

    feat: refactor COSBalance to use Riverpod for state management; add TransactionFlowCard widget for better separation of concerns

--- lib/src/features/dashboard/presentation/widgets/balance.dart
+++ lib/src/features/dashboard/presentation/widgets/balance.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:spendit/src/common/common.dart';
 
-class COSBalance extends StatelessWidget {
+import '../../../../common/common.dart';
+import '../../../transaction/presentation/providers/expense.dart';
+import '../../../transaction/presentation/providers/income.dart';
+import 'transaction_flow_card.dart';
+
+class COSBalance extends ConsumerWidget {
   const COSBalance({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Container(
       margin: EdgeInsets.all(4.w),
       padding: EdgeInsets.all(16.w),
@@ -20,84 +25,68 @@ class COSBalance extends StatelessWidget {
           ],
         ),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        spacing: 12.h,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Balance',
-                style: kRegularTextStyle.copyWith(
-                  fontSize: 18.sp,
-                  color: context.colorScheme.primary.withValues(alpha: .6),
-                ),
-              ),
-              Text(1500000.currency, style: kBoldTextStyle.copyWith(fontSize: 28.sp)),
-            ],
-          ),
-          Container(
-            constraints: BoxConstraints(maxWidth: context.deviceSize.width),
-            child: Row(
-              spacing: 12.w,
-              children: [
-                Expanded(child: TransactionFlowCard(type: TransactionType.income, value: 1000000)),
-                Expanded(child: TransactionFlowCard(type: TransactionType.expense, value: 1000000)),
-              ],
-            ),
-          ),
-        ],
-      ),
+      child: child(ref),
     );
+  }
+
+  Widget child(WidgetRef ref) {
+    final income = ref.watch(incomeStateProvider(date: now));
+    final expense = ref.watch(expenseStateProvider(date: now));
+    if (income.isLoading || expense.isLoading) {
+      return COSBalanceData(balance: 1500000, income: 1000000, expense: 1000000).skeleton();
+    }
+    if (income.hasError || expense.hasError) {
+      return Center(child: Text('Error Occured'));
+    }
+    final tIncome = income.hasValue ? income.valueOrNull!.fold(0, (v0, v1) => v0 + v1.value) : 0;
+    final tExpense = expense.hasValue ? expense.valueOrNull!.fold(0, (v0, v1) => v0 + v1.value) : 0;
+    final tBalance = tIncome - tExpense;
+    return COSBalanceData(balance: tBalance, income: tIncome, expense: tExpense);
   }
 }
 
-class TransactionFlowCard extends StatelessWidget {
-  const TransactionFlowCard({super.key, required this.value, required this.type});
+class COSBalanceData extends StatelessWidget {
+  const COSBalanceData({
+    super.key,
+    required this.balance,
+    required this.income,
+    required this.expense,
+  });
 
-  final TransactionType type;
-  final int value;
+  final int balance;
+  final int income;
+  final int expense;
 
   @override
   Widget build(BuildContext context) {
-    final title = switch (type) {
-      TransactionType.expense => 'Expense',
-      TransactionType.income => 'Income',
-    };
-
-    final symbol = switch (type) {
-      TransactionType.expense => '-',
-      TransactionType.income => '+',
-    };
-
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(12),
-      child: Container(
-        padding: EdgeInsets.all(12.w),
-        decoration: BoxDecoration(
-          color: Colors.white.withAlpha(50),
-          borderRadius: BorderRadius.circular(12.r),
-        ),
-        child: Column(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 12.h,
+      children: [
+        Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          spacing: 8.h,
           children: [
             Text(
-              title,
-              style: kMediumTextStyle.copyWith(
+              'Balance',
+              style: kRegularTextStyle.copyWith(
+                fontSize: 18.sp,
                 color: context.colorScheme.primary.withValues(alpha: .6),
               ),
             ),
-            Text(
-              '$symbol ${value.currency}',
-              style: kBoldTextStyle.copyWith(fontSize: 14.sp),
-              overflow: TextOverflow.ellipsis,
-            ),
+            Text(balance.currency, style: kBoldTextStyle.copyWith(fontSize: 28.sp)),
           ],
         ),
-      ),
+        Container(
+          constraints: BoxConstraints(maxWidth: context.deviceSize.width),
+          child: Row(
+            spacing: 12.w,
+            children: [
+              Expanded(child: TransactionFlowCard(type: TransactionType.income, value: income)),
+              Expanded(child: TransactionFlowCard(type: TransactionType.expense, value: expense)),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/src/features/dashboard/presentation/widgets/transaction_flow_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../common/common.dart';
+
+class TransactionFlowCard extends StatelessWidget {
+  const TransactionFlowCard({super.key, required this.value, required this.type});
+
+  final TransactionType type;
+  final int value;
+
+  @override
+  Widget build(BuildContext context) {
+    final title = switch (type) {
+      TransactionType.expense => 'Expense',
+      TransactionType.income => 'Income',
+    };
+
+    final symbol = switch (type) {
+      TransactionType.expense => '-',
+      TransactionType.income => '+',
+    };
+
+    String v = value == 0 ? '-' : '$symbol ${value.currency}';
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        padding: EdgeInsets.all(12.w),
+        decoration: BoxDecoration(
+          color: Colors.white.withAlpha(50),
+          borderRadius: BorderRadius.circular(12.r),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          spacing: 8.h,
+          children: [
+            Text(
+              title,
+              style: kMediumTextStyle.copyWith(
+                color: context.colorScheme.primary.withValues(alpha: .6),
+              ),
+            ),
+            Text(
+              v,
+              style: kBoldTextStyle.copyWith(fontSize: 14.sp),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
