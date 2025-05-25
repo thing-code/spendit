e24f456a9260953ff24eff18ac41fe5e5f7a879c
fahmi <devfloody@gmail.com>
2025-05-25 16:42:12 +0700

    pindah modal datepicker

--- packages/spendit_core/lib/src/utils/modal.dart
+++ packages/spendit_core/lib/src/utils/modal.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:spendit_core/spendit_core.dart';
 import 'package:spendit_core/src/widgets/bottom_sheet.dart';
 
+/// Custom modal bottom sheet with default settings.
 Future<T?> openBottomSheet<T>(
   BuildContext context, {
   required Widget Function(BuildContext context) builder,
@@ -11,11 +13,47 @@ Future<T?> openBottomSheet<T>(
 }) => showModalBottomSheet(
   context: context,
   isDismissible: isDismissable,
-  builder:
-      (context) => DefaultBottomSheet(
-        builder: builder,
-        showCloseIcon: showCloseIcon,
-        height: height,
-        title: title,
-      ),
+  builder: (context) => DefaultBottomSheet(
+    builder: builder,
+    showCloseIcon: showCloseIcon,
+    height: height,
+    title: title,
+  ),
 );
+
+/// Open a date picker based on custom bottom sheet.
+Future<DateTime?> openSpendItDatePicker(
+  BuildContext context, {
+  required DateTime firstDate,
+  required DateTime lastDate,
+  DateTime? initialDate,
+}) {
+  DateTime? selectedDate;
+  return openBottomSheet(
+    context,
+    // title: 'Pilih Tanggal',
+    // showCloseIcon: true,
+    height: context.deviceHeight * .6,
+    builder: (context) {
+      return Column(
+        children: [
+          CalendarDatePicker(
+            initialDate: initialDate ?? DateTime.now(),
+            firstDate: firstDate,
+            lastDate: lastDate,
+            onDateChanged: (value) {
+              selectedDate = value;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: SpendItButton.primary(
+              text: 'Pilih',
+              onPressed: () => Navigator.pop(context, selectedDate),
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
