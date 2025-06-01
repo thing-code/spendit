95ec624506b5acbecde37efd8839887991bf92a1
fahmi <devfloody@gmail.com>
2025-06-01 12:16:25 +0700

    feat: add initial page widgets and balance controller
    - Create placeholder pages for home, transactions and transaction details
    - Implement balance controller to calculate income vs expenses

---
+++ apps/spendit/lib/src/features/home/presentation/controllers/balance_controller.dart
@@ -0,0 +1,34 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:spendit_core/spendit_core.dart';
+import 'package:spendit_remake/src/features/transactions/domain/models/transaction_model.dart';
+import 'package:spendit_remake/src/features/transactions/presentation/controllers/transaction_provider.dart';
+
+part 'balance_controller.g.dart';
+
+@riverpod
+class BalanceController extends _$BalanceController {
+  @override
+  FutureOr<int> build() async {
+    final expenses = await ref
+        .read(transactionRepositoryProvider)
+        .readByType(TransactionType.expense);
+    final incomes = await ref
+        .read(transactionRepositoryProvider)
+        .readByType(TransactionType.income);
+
+    if (expenses is LocalResponseFailure || incomes is LocalResponseFailure) {
+      return 0;
+    }
+
+    final totalExpenses = (expenses as LocalResponseSuccess<List<TransactionModel>>).data.fold(
+      0,
+      (previousValue, element) => previousValue + element.amount,
+    );
+    final totalIncomes = (incomes as LocalResponseSuccess<List<TransactionModel>>).data.fold(
+      0,
+      (previousValue, element) => previousValue + element.amount,
+    );
+    
+    return totalIncomes - totalExpenses;
+  }
+}
---
+++ apps/spendit/lib/src/features/transactions/presentation/pages/transaction_detail_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class TransactionDetailPage extends StatelessWidget {
+  const TransactionDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
