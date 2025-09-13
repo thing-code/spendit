d8d20224558be311a04c34ec2c63bc1c93b08e96
fahmi <fahmiismail.dev@gmail.com>
2025-09-13 08:40:27 +0700

    add splash and icons

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart' show initializeDateFormatting;
+import 'package:spendit/src/common/common.dart';
 
 Future<void> main() async {
-  runZonedGuarded(() async {
-    WidgetsFlutterBinding.ensureInitialized();
+  runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
 
-    /// Untuk mengatur status bar menjadi transparan
-    SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(statusBarColor: Colors.transparent),
-    );
+      /// Untuk mengatur status bar menjadi transparan
+      SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+      );
 
-    await initializeDateFormatting('id_ID');
+      await initializeDateFormatting('id_ID');
 
-    runApp(ProviderScope(child: const MainApp()));
-  }, (error, stack) {});
+      runApp(ProviderScope(child: const MainApp()));
+    },
+    (error, stack) {
+      SiLogger.error('AppError : ${error.toString()}');
+    },
+  );
 }
 
 class MainApp extends StatelessWidget {
---
+++ lib/src/common/common.dart
@@ -0,0 +1,6 @@
+export 'configs/configs.dart';
+export 'constants/constants.dart';
+export 'extensions/extensions.dart';
+export 'helpers/helpers.dart';
+export 'themes/themes.dart';
+export 'widgets/widgets.dart';
