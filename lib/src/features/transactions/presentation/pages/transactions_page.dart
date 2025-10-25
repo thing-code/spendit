e1d2b76e94a1a5ce1e216fd2ba2db4e6d5f1977f
Thing Code <fahmiismail.dev@gmail.com>
2025-10-25 09:40:29 +0700

    Refactor Dependency

--- lib/src/features/transactions/presentation/pages/transactions_page.dart
+++ lib/src/features/transactions/presentation/pages/transactions_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 import '../../../../core/core.dart';
+import '../../domain/models/transactions.dart';
 
 class TransactionsPage extends ConsumerWidget {
   const TransactionsPage({super.key});
@@ -11,7 +12,46 @@ class TransactionsPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: SiAppBar(title: Text('Transactions'), canPop: false),
-      body: Column(children: [Text(DateFormat.MMMM().format(DateTime.now()))]),
+      body: Column(
+        children: [
+          SfCircularChart(
+            palette: ExpenseCategory.values.reversed
+                .toList()
+                .map((e) => expenseCategoryColors[e]!)
+                .toList(),
+            legend: Legend(
+              isVisible: true,
+              position: LegendPosition.bottom,
+              overflowMode: LegendItemOverflowMode.wrap,
+            ),
+            series: [
+              RadialBarSeries<Expense, String>(
+                dataSource: ExpenseCategory.values.reversed
+                    .toList()
+                    .asMap()
+                    .entries
+                    .map(
+                      (e) => Expense(
+                        amount: 10000 + ((e.key + 1) * 10000),
+                        category: e.value,
+                        createdAt: DateTime.now(),
+                      ),
+                    )
+                    .toList(),
+                gap: '5%',
+                radius: '100%',
+                innerRadius: '32%',
+                legendIconType: LegendIconType.circle,
+                trackColor: SiColors.card,
+                xValueMapper: (datum, index) => datum.category.title,
+                yValueMapper: (datum, index) => datum.amount,
+                cornerStyle: CornerStyle.bothCurve,
+                maximumValue: 200000,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
